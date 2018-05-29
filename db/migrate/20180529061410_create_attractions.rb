cd24b706d27075783b100e1da217bf2bc33ea9db
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-05-29 06:25:46 +0000

    added code to crete columns for attractions table

--- db/migrate/20180529061410_create_attractions.rb
+++ db/migrate/20180529061410_create_attractions.rb
@@ -1,4 +1,10 @@
 class CreateAttractions < ActiveRecord::Migration
   def change
+    create_table :attractions do |t|
+      t.string :name
+      t.string :description
+      t.integer :user_id
+      t.integer :city_id
+    end
   end
 end
