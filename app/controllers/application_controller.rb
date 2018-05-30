aa2b8b995b4b4d04697f953eff50523ba39a36f6
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-05-30 18:05:20 +0000

    reordered routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,15 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/login' do
-    erb :'users/login'
-  end
-
   get '/signup' do
     erb :'users/signup'
+  end
+
+  post '/signup' do
+    
+  end
+
+  get '/login' do
+    erb :'users/login'
   end
 end
