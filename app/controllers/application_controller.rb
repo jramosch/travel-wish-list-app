fa7991b5ec1461bce5794fc2bb70f073a512fbff
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-05-30 17:15:49 +0000

    added login & signup routes to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  get '/login' do
+    erb :'users/login'
+  end
+
+  get '/signup' do
+    erb :'users/signup'
+  end
 end
