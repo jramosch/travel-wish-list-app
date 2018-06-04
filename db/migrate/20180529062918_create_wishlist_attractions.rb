ed893c62802a67e7927497768902b71f0745287d
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-06-04 18:53:56 +0000

    corrected data type typo in wishlist attraction migration

--- db/migrate/20180529062918_create_wishlist_attractions.rb
+++ db/migrate/20180529062918_create_wishlist_attractions.rb
@@ -2,7 +2,7 @@ class CreateWishlistAttractions < ActiveRecord::Migration
   def change
     create_table :wishlist_attractions do |t|
       t.integer :wishlist_id
-      t.tnteger :attraction_id
+      t.integer :attraction_id
     end
   end
 end
