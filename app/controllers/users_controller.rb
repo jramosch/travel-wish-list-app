4cf0f653843c90eed4adf56df8310370789b1393
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-06-04 20:47:21 +0000

    added two routes to users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+
+  get '/users' do
+    @users = User.all
+    erb :'users/index'
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
+
+end
