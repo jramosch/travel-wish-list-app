d7d606154bb9275ea8e9c5af5d450a5a09100352
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-06-06 03:08:17 +0000

    updated wishlist edit route to check if user is logged in and owns wishlist

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -4,12 +4,14 @@ class WishlistsController < ApplicationController
     erb :'wishlists/index'
   end
 
+
+
   get '/wishlists/:id/edit' do
     @wishlist = Wishlist.find(params[:id])
     if logged_in && current_user.id == @wishlist.user_id
       erb :'wishlists/edit'
     else
-      redirect to "/wishlists/#{params[:id]}"
+      redirect to "/wishlists"
     end
   end
 
