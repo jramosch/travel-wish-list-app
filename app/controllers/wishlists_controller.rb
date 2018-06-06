26171d20f35b8a83247cddee262c67df32dc9e72
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-06-06 04:06:01 +0000

    updated post route for wishlist edit

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -4,7 +4,17 @@ class WishlistsController < ApplicationController
     erb :'wishlists/index'
   end
 
-
+  post '/wishlists/:id' do
+    wishlist = Wishlist.find(params[:id])
+    wishlist.update(params["wishlist"])
+    if !params["attraction"]["name"].empty?
+      new_attraction = Attraction.create(name: params["attraction"]["name"])
+      wishlist.attractions << new_attraction
+      if !params["city"]["name"].empty?
+        new_attraction.city = City.create(name: params["city"]["name"])
+      end
+    end
+  end
 
   get '/wishlists/:id/edit' do
     @wishlist = Wishlist.find(params[:id])
