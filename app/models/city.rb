bd14a5946e8a29a717d8a8af77f163b9293509b9
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-06-15 16:29:21 +0000

    required slugify file

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,7 +1,9 @@
+require_relative 'concerns/slugify'
+
 class City < ActiveRecord::Base
-  include Slugify::NameMethods
+  include Slugify::InstanceMethods
   extend Slugify::ClassMethods
-  
+
   belongs_to :user
   has_many :attractions
 end
