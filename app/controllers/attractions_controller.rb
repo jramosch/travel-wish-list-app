b583de26419d0f67ed2010fd57e90716dc440795
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-06-15 14:48:58 +0000

    added in post method for creation of new attraction

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -9,6 +9,15 @@ class AttractionsController < ApplicationController
     erb :'attractions/create'
   end
 
+  post '/attractions' do
+    attraction = Attraction.create(params["attraction"])
+    if !params["city"]["name"].empty?
+      new_city = City.create(name: params["city"]["name"], user_id: "#{current_user.id}")
+      attraction.update(city_id: "#{new_city.id}")
+    end
+    redirect to "/attractions/#{attraction.id}"
+  end
+
   get '/attractions/:id' do
     @attraction = Attraction.find(params[:id])
     @current_user = current_user
