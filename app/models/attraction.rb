50035699cfd2631f4cf9365ef4fd8d3e304eed1d
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-06-15 16:46:55 +0000

    included and extended slugify methods

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,4 +1,9 @@
+require_relative 'concerns/slugify'
+
 class Attraction < ActiveRecord::Base
+  include Slugify::NameMethods
+  extend Slugify::ClassMethods
+  
   belongs_to :user
   belongs_to :city
   has_many :wishlist_attractions
