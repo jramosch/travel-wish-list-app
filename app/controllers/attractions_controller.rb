9ea1997ac3bd91148362bd838f6ea5b09c46f336
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-06-16 00:47:09 +0000

    updated with flash messages for creation, editing, and deleting of an attraction

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -16,6 +16,7 @@ class AttractionsController < ApplicationController
       new_city = City.create(name: params["city"]["name"], user_id: current_user.id)
       attraction.update(city_id: new_city.id)
     end
+    flash[:message] = "attraction created"
     redirect to "/attractions/#{attraction.slug}"
   end
 
@@ -27,6 +28,7 @@ class AttractionsController < ApplicationController
   post '/attractions/:slug' do
     attraction = Attraction.find_by_slug(params[:slug])
     attraction.update(params["attraction"])
+    flash[:message] = "attraction updated"
     redirect to "/attractions/#{attraction.slug}"
   end
 
@@ -38,6 +40,7 @@ class AttractionsController < ApplicationController
   delete '/attractions/:slug/delete' do
     attraction = Attraction.find_by_slug(params[:slug])
     attraction.delete
+    flash[:message] = "attraction deleted"
     redirect to "/home"
   end
 
