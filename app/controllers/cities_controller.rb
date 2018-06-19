29fdea6fea46af77db9d75ababe28de934782771
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-06-19 15:38:39 +0000

    added flash message to confirm city name update

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,4 +1,5 @@
 class CitiesController < ApplicationController
+  use Rack::Flash
 
   get '/cities' do
     @cities = City.all
@@ -13,6 +14,7 @@ class CitiesController < ApplicationController
   post '/cities/:slug' do
     city = City.find_by_slug(params[:slug])
     city.update(name: params["name"])
+    flash[:message] = "City name updated."
     redirect to "/cities/#{city.slug}"
   end
 end
